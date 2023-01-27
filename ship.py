92bfbeaa6accbb58ef9f56bc0af711d8793e47ac
dilrajs616 <dilraj082@gmail.com>
2023-01-27 20:11:18 +0530

    Adjusted Screen Collision of Ship

--- ship.py
+++ ship.py
@@ -28,16 +28,16 @@ class Ship:
 
     def update(self):
         '''Update the ship's position (not the rect) based on the movement flags.'''
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             # self.rect.x += 1
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
             # self.rect.x -= 1
-        if self.moving_up:
+        if self.moving_up and self.rect.top > 0:
             self.y -= self.settings.ship_speed
             # self.rect.y -= 1
-        if self.moving_down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.y += self.settings.ship_speed
             # self.rect.y += 1
 
