1dab9e3c3e01588513c556186b82df46f3f31781
dilrajs616 <dilraj082@gmail.com>
2023-01-29 18:21:33 +0530

    Displayed lives and finished project

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,7 +74,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
-            self.sb.prep_level()            
+            self.sb.prep_level()
+            self.sb.prep_ships()            
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -198,8 +199,9 @@ class AlienInvasion:
     def _ship_hit(self):
         '''Respond to the ship being hit by an alien.'''
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
